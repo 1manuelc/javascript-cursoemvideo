5fd97645f34483405b54b81d6497f8da193942e8
Manuel Carlos <manuelwn21@gmail.com>
2023-01-13 03:05:15 -0300

    estudos++

---
+++ Modulo D/Aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 5
+
+console.log(`a velocidade do seu carro é ${vel}km/h`)
+
+if(vel > 60)
+    console.log("você passou do limite de velocidade permitido. MULTADO!")
+
+console.log("dirija sempre usando cinto de segurança")
